1aa85381752f2a3bb449e245b7d1b57000b27719
Kelly Johnson <kellyjohnson@Kellys-MacBook-Pro.local>
2017-03-31 12:27:35 -0700

    opted for restart of project

---
+++ Bout Time/RoundedCornersLAbel.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedCornersLAbel.swift
+//  Bout Time
+//
+//  Created by Kelly Johnson on 3/31/17.
+//  Copyright © 2017 leme group. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundCorners: UILabel {
+    @IBInspectable var cornerRadius: CGFloat = 0    {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+ //   @IBInspectable var verticalPad: CGFloat = 0 {
+ //
+ //   }
+ //   @IBInspectable var horizontalPad: CGFloat = 0 {
+ //
+ //   }
+    
+}
