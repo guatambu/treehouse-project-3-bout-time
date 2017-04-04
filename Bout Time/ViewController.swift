cebf5cc0bbc0af5bb2961775b8581fe2c2807656
Kelly Johnson <kellyjohnson@Kellys-MacBook-Pro.local>
2017-04-04 14:25:25 -0700

    set up basic game play via shake gesture, but all buttons need to be rewired and made live

--- Bout Time/ViewController.swift
+++ Bout Time/ViewController.swift
@@ -12,21 +12,30 @@ import AudioToolbox
 
 class ViewController: UIViewController {
 // let's set up our necessary variables and respective instances
-    let roundssPerRound = 6
+    let roundsPerGame = 6
     var roundsPlayed = 0
     var correctAnswers = 0
     var randomIndexNumber: Int = 0
     var timelineSource = TimelineSource()
-    var currentRound = TimelineEvent()
+    //var currentRound = TimelineEvent()
     var removedArrayItem = TimelineEvent()
     var correctDing: SystemSoundID = 0
     var incorrectBuzz: SystemSoundID = 0
     var gameSound1: SystemSoundID = 0
     var claps1: SystemSoundID = 0
+    var event1 = TimelineEvent()
+    var event2 = TimelineEvent()
+    var event3 = TimelineEvent()
+    var event4 = TimelineEvent()
+    
     
     
     
 // timeline event options display UIView IBOutlets wiring
+    @IBOutlet weak var view1: ViewRoundedCorners!
+    @IBOutlet weak var view2: ViewRoundedCorners!
+    @IBOutlet weak var view3: ViewRoundedCorners!
+    @IBOutlet weak var view4: ViewRoundedCorners!
     @IBOutlet weak var timelineDisplay1: UILabel!
     @IBOutlet weak var timelineDisplay2: UILabel!
     @IBOutlet weak var timelineDisplay3: UILabel!
@@ -37,6 +46,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var playAgain: UIButton!
     @IBOutlet weak var incorrectNextRound: UIButton!
     @IBOutlet weak var correctNextRound: UIButton!
+    @IBOutlet weak var timerDisplay: UILabel!
 
 // timeline event options buttons in UIViews IBActions wiring
     
@@ -58,18 +68,25 @@ class ViewController: UIViewController {
     }
 
 // Next Round Correct Button wiring
-    @IBAction func nextRoundCorrectButton(_ sender: UIButton) {
-        playCorrectDing()
+    @IBAction func correctOnToNextRound() {
+       // nextRound()
+        
     }
 
-// Next Round Incorrect Button wiring 
-    @IBAction func nextRoundIncorrectButton(_ sender: UIButton) {
-        playIncorrectBuzz()
+
+// Next Round Incorrect Button wiring
+    @IBAction func failOnToNextRound() {
+        //nextRound()
+        
     }
+
+    
 
 // Play Again Button wiring
-    @IBAction func playAgainButton(_ sender: UIButton) {
+    @IBAction func playAgainButton() {
+        
     }
+
 
 
     override func viewDidLoad() {
@@ -84,6 +101,11 @@ class ViewController: UIViewController {
         playAgain.isHidden = true
         correctNextRound.isHidden = true
         incorrectNextRound.isHidden = true
+        view1.isHidden = false
+        view2.isHidden = false
+        view3.isHidden = false
+        view4.isHidden = false
+        timerDisplay.isHidden = false
         loadGameStartSound()
         playGameStartSound()
         //downFull.isHidden = false
@@ -103,13 +125,14 @@ class ViewController: UIViewController {
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if event?.subtype == UIEventSubtype.motionShake {
-            // run code to check for correct answer
+            //checkAnswer()
+            nextRound()
+            roundsPlayed += 1
         }
     }
     
     //MARK: these funcitons are not very DRY but they get the job done.  to be refactored
     func eventGeneratorTimlineDisplay1() {
-        var event1: TimelineEvent
         randomIndexNumber = timelineSource.randomIndexNumberGenerator()
         event1 = timelineSource.randomEvent(at: randomIndexNumber)
         removedArrayItem = timelineSource.arrayItemRemover(at: randomIndexNumber)
@@ -118,7 +141,6 @@ class ViewController: UIViewController {
     }
     
     func eventGeneratorTimlineDisplay2() {
-        var event2: TimelineEvent
         randomIndexNumber = timelineSource.randomIndexNumberGenerator()
         event2 = timelineSource.randomEvent(at: randomIndexNumber)
         removedArrayItem = timelineSource.arrayItemRemover(at: randomIndexNumber)
@@ -127,7 +149,6 @@ class ViewController: UIViewController {
     }
     
     func eventGeneratorTimlineDisplay3() {
-        var event3: TimelineEvent
         randomIndexNumber = timelineSource.randomIndexNumberGenerator()
         event3 = timelineSource.randomEvent(at: randomIndexNumber)
         removedArrayItem = timelineSource.arrayItemRemover(at: randomIndexNumber)
@@ -136,7 +157,6 @@ class ViewController: UIViewController {
     }
     
     func eventGeneratorTimlineDisplay4() {
-        var event4: TimelineEvent
         randomIndexNumber = timelineSource.randomIndexNumberGenerator()
         event4 = timelineSource.randomEvent(at: randomIndexNumber)
         removedArrayItem = timelineSource.arrayItemRemover(at: randomIndexNumber)
@@ -144,7 +164,94 @@ class ViewController: UIViewController {
         //playAgainButton.isHidden = true
     }
     
+    func displayScore() {
+        
+    }
+    
+    func checkAnswer() {
+        roundsPlayed += 1
+        timerDisplay.isHidden = true
+        
+        let eventOrder1 = event1.order
+        let eventOrder2 = event2.order
+        let eventOrder3 = event3.order
+        let eventOrder4 = event4.order
+        
+        if (eventOrder1 < eventOrder2) && (eventOrder2 < eventOrder3) && (eventOrder3 < eventOrder4) {
+            //loadCorrectDing()
+            //playCorrectDing()
+            correctAnswers += 1
+            correctNextRound.isHidden = false
 
+        } else {
+            //loadIncorrectBuzz()
+            //playIncorrectBuzz()
+            incorrectNextRound.isHidden = false
+        }
+    }
+    
+    func nextRound() {
+        if roundsPlayed == roundsPerGame {
+            // Game = over
+            displayFinalScore()
+        } else {
+            // Game continues
+            eventGeneratorTimlineDisplay1()
+            eventGeneratorTimlineDisplay2()
+            eventGeneratorTimlineDisplay3()
+            eventGeneratorTimlineDisplay4()
+       }
+        
+    }
+    
+    func displayFinalScore() {
+        yourScore.isHidden = false
+        finalScore.isHidden = false
+        finalScore.text = "\(correctAnswers)/\(roundsPerGame)"
+        playAgain.isHidden = false
+        view1.isHidden = true
+        view2.isHidden = true
+        view3.isHidden = true
+        view4.isHidden = true
+        timerDisplay.isHidden = true
+        instructions.isHidden = true
+        loadEndGameSound()
+        playEndGameSound()
+    }
+
+    func playNewGame() {
+        roundsPlayed = 0
+        correctAnswers = 0
+        timelineSource = TimelineSource()
+        event1 = TimelineEvent()
+        event2 = TimelineEvent()
+        event3 = TimelineEvent()
+        event4 = TimelineEvent()
+        eventGeneratorTimlineDisplay1()
+        eventGeneratorTimlineDisplay2()
+        eventGeneratorTimlineDisplay3()
+        eventGeneratorTimlineDisplay4()
+        yourScore.isHidden = true
+        finalScore.isHidden = true
+        playAgain.isHidden = true
+        correctNextRound.isHidden = true
+        incorrectNextRound.isHidden = true
+        view1.isHidden = false
+        view2.isHidden = false
+        view3.isHidden = false
+        view4.isHidden = false
+        timerDisplay.isHidden = false
+        loadGameStartSound()
+        playGameStartSound()
+        //downFull.isHidden = false
+        //upFull.isHidden = false
+        //upHalf1.isHidden = false
+        //upHalf2.isHidden = false
+        //downHalf1.isHidden = false
+        //downHalf2.isHidden = false
+
+    }
+    
     
     // audio sounds functions setup.
     // i admit here i brought in some sounds from the previous project.
